90df3f4450d844d72476b87239dcb26a31dfb97c
Prem Prakash Sharma <premprakashsharma9211@gmail.com>
2024-06-16 14:22:38 +0530

    init

--- Sorting Algorithms/Bubble Sort/cpp/bubble_sort.cpp
+++ Sorting Algorithms/Bubble Sort/cpp/bubble_sort.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int n;
-
-void bubble_Sort(int array[]){
-
+void bubbleSort(int array[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j + 1]);
+            }
+        }
+    }
 }
 
-void main(){
-  return 0
+void printArray(int array[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int array[] = {5, 3, 8, 6, 2};    
+    int n = sizeof(array) / sizeof(array[0]);
+    
+    bubbleSort(array, n);
+
+    cout << "Sorted array: ";
+    printArray(array, n);
+    
+    return 0;
 }
